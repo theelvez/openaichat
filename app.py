e7b2070b060b5dfc54e34a87d65f2de8d20eeba3
Jonathan Morrison <jonmorri@msn.com>
2023-02-02 18:54:01 -0800

    update html display

--- app.py
+++ app.py
@@ -1,45 +1,45 @@
 from flask import Flask, render_template, request
 import json
-import requests
 import os
 import logging
+import openai
 
 app = Flask(__name__)
 
 @app.route("/submit", methods=['POST'])
 def openai_post():
     model = request.form['model']
-    prompt = request.form['prompt']
     temperature = float(request.form['temperature'])
-
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + "sk-" + os.getenv("OA_K_1") + os.getenv("OA_K_2") 
-    }
+    prompt_text = request.form['prompt']
 
     # Set up a logger
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
+    logger.error("Sending prompt: " + prompt_text)
 
-    data = json.dumps({
-        "prompt": prompt,
-        "temperature": temperature,
-        "max_tokens": 4000
-    })
-
-    logger.error("Sending prompt: " + prompt)
-
-    url = "https://api.openai.com/v1/engines/{}/completions".format(model)
-    response = requests.post(url, headers=headers, data=data)
-    response_text = json.loads(response.text)['choices'][0]['text']
+    openai.api_key = "sk-" + os.getenv("OA_K_1") + os.getenv("OA_K_2") 
     
-    logger.error("Response: " + response_text)
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt_text,
+        temperature=0.7,
+        max_tokens=4000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
 
-    return render_template('index.html', prompt_text=prompt, response_text=response_text)
+    response_text = response['choices'][0]['text']
+    
+    logger.error("response: " + response_text)
 
-@app.route("/openai", methods=['GET'])
+    prompt_text += response_text + "\n\nYou: "
+
+    return render_template('index.html', prompt_text=prompt_text)
+
+@app.route("/", methods=['GET'])
 def openai_get():
-    return render_template('index.html')
+    return render_template('index.html', prompt_text="You: ")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=80, debug=True)
